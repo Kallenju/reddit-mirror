96fb031b36e0fd97468e7f4e27ddb6b41fe9f8df
Konstantin Solovev <kostyasolovyov54@gmail.com>
2023-01-14 22:50:53 +0400

    add support for node 16

--- src/server/retrievingMirrorRedditToken.ts
+++ src/server/retrievingMirrorRedditToken.ts
@@ -1,20 +1,27 @@
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.${process.env.NODE_ENV}.env`),
+});
+
+const { URI, PORT } = process.env;
+
 import { Request } from 'express';
 import RedditToken from '../interfaces/RedditToken';
+import fetch from 'node-fetch';
 
 export default async function retrievingMirrorRedditToken(
   request: Request
 ): Promise<string | void> {
-  const response: Response = await fetch(
-    `https://www.reddit.com/api/v1/access_token`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${btoa(`${__CLIENT_ID}:${__CLIENT_SECRET}`)}`,
-      },
-      body: `grant_type=authorization_code&code=${request.query.code}&redirect_uri=http://localhost:${__PORT}/auth`,
-    }
-  );
+  const response = await fetch(`https://www.reddit.com/api/v1/access_token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${btoa(`${__CLIENT_ID}:${__CLIENT_SECRET}`)}`,
+    },
+    body: `grant_type=authorization_code&code=${request.query.code}&redirect_uri=${URI}:${PORT}}/auth`,
+  });
 
   if (response.ok) {
     const token: RedditToken = (await response.json()) as RedditToken;
